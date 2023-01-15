f8f25383f0eaa0bccd95cd6c024696295f42fc69
PedroPerpetua <pedro.perpetua@sapo.pt>
2023-01-15 08:39:04 +0000

    Fixed migration test.

--- app/core/tests/test_migrations.py
+++ app/core/tests/test_migrations.py
@@ -8,7 +8,7 @@ class TestMigrations(TestCase):
 
     def test_load_admin_migration(self) -> None:
         """Test that a superuser was created on migrations."""
-        user_filter = get_user_model().objects.filter(is_staff=True)
+        user_filter = get_user_model().objects.filter(is_staff=True, is_superuser=True)
         self.assertEqual(1, user_filter.count())
         user = user_filter.get()
         # Because the migrations are made BEFORE the tests, we can't actually mock them - check the originals.
