8fe96be79b0ebbc8d69cc49c874aa35ec4d19efc
PedroPerpetua <pedro.perpetua@sapo.pt>
2023-01-26 15:28:36 +0000

    Fixed mypy issue.

--- app/core/exceptions/formatter.py
+++ app/core/exceptions/formatter.py
@@ -1,9 +1,17 @@
 from dataclasses import asdict
-from drf_standardized_errors.formatter import ExceptionFormatter as BaseExceptionFormatter
-from drf_standardized_errors.types import ErrorResponse
+from drf_standardized_errors.formatter import ExceptionFormatter as BaseExceptionFormatter  # type: ignore
+from drf_standardized_errors.types import ErrorResponse  # type: ignore
 from core.utilities.types import JSON
 
 
-class ExceptionFormatter(BaseExceptionFormatter):
-    def format_error_response(self, error_response: ErrorResponse) -> JSON:
+"""
+TODO: fix drf_standardized_errors types.
+The module is currently missing it's `py.typed` file. An issue has been raised and the owner is on it. Once the issue
+is fixed, the module should be updated and all `# type: ignore`s removed.
+Issue: https://github.com/ghazi-git/drf-standardized-errors/issues/32
+"""
+
+
+class ExceptionFormatter(BaseExceptionFormatter):  # type: ignore
+    def format_error_response(self, error_response: ErrorResponse) -> JSON:  # type: ignore
         return {"errors": [asdict(err) for err in super().get_errors()]}
