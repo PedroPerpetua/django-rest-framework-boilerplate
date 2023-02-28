3f412bf50c8c68a5525ca9dc921bbbad510731f9
PedroPerpetua <pedro.perpetua@sapo.pt>
2023-02-28 21:49:25 +0000

    Closes #27; added a type alias for UsesQuerySet.

--- app/core/utilities/types.py
+++ app/core/utilities/types.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Any, TypeVar
+from typing import TYPE_CHECKING, Any, Generic, TypeAlias, TypeVar
 from django.contrib.auth.models import AbstractBaseUser
 from django.db.models import Model
 from django.urls.resolvers import URLPattern, URLResolver
@@ -6,14 +6,14 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.views import APIView
 
 
-JSON = Any
+JSON: TypeAlias = Any
 """
 Type alias for common JSON objects. We type alias it as JSON because recursive types are not 100% working, and because
 JSON objects are very mutable by themselves (can be dictionaries, lists, etc).
 """
 
 
-JSON_BASE = dict[str, Any] | list[Any] | Any
+JSON_BASE: TypeAlias = dict[str, Any] | list[Any] | Any
 """
 Alternative JSON definition useful for functions that manipulate JSON: functions that take a JSON object and return
 the same object type.
@@ -26,14 +26,23 @@ GenericModel = TypeVar("GenericModel", bound=Model, covariant=True)
 GenericUser = TypeVar("GenericUser", bound=AbstractBaseUser)
 """Type alias for Generic classes that use some kind of User."""
 
-URLPatternsList = list[URLPattern | URLResolver]
+URLPatternsList: TypeAlias = list[URLPattern | URLResolver]
 """Type alias specifically for urlpatterns lists."""
 
 
 """Type alias for Mixins."""
 if TYPE_CHECKING:
-    GenericViewMixin = GenericAPIView
-    APIViewMixin = APIView
+    GenericViewMixin: TypeAlias = GenericAPIView
+    APIViewMixin: TypeAlias = APIView
 else:
     GenericViewMixin = object
     APIViewMixin = object
+
+
+"""Type alias for the generic UsesQueryset."""
+if TYPE_CHECKING:
+    from rest_framework.generics import UsesQuerySet as BaseUsesQuerySet
+
+    UsesQuerySet: TypeAlias = BaseUsesQuerySet
+else:
+    UsesQuerySet = Generic
